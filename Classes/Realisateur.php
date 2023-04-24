b692660d1111c2506fea6df9baf9c0f9a369238f
angalland <angalland@laposte.net>
2023-04-24 15:02:52 +0200

    creation d'une classe Personne

--- Classes/Realisateur.php
+++ Classes/Realisateur.php
@@ -1,76 +1,21 @@
 <?php
 
-class Realisateur {
+class Realisateur extends Personne {
 
     // argument
 
-    private array $_film;
+     private array $_film;
 
     // constructeur
 
     public function __construct(){
         $this->_film = [];
-    
+        parent::__construct($nom, $prenom, $duree, $dateNaissance);
+    }
+
+    // ajouter film
+    public function addFilm(Film $film){
+        $this->_film[] = $film;
     }
 }
 
-class Personne extends Realisateur {
-
-    // argument 
-
-    protected string $_nom;
-    protected string $_prenom;
-    protected string $_genre;
-    protected DateTime $_dateNaissance;
-
-  
-
-    public function __construct (array $film, string $nom, string $prenom, string $genre, string $dateNaissance){
-        parent::__construct($film);
-        $this->_nom = $nom;
-        $this->_prenom = $prenom;
-        $this->_genre = $genre;
-        $this->_dateNaissance = new DateTime ($dateNaissance);
-
-    }
-
-    // Getter et Setter 
-
-    public function getNom() : string {
-        return $this->_nom;
-    }
-
-    public function setNom($nom) : string {
-        return $this->_nom = $nom;
-    }
-
-    public function getPrenom() : string {
-        return $this->_prenom;
-    }
-
-    public function setPrenom($prenom) : string {
-        return $this->_prenom = $prenom;
-    }
-
-    public function getGenre() : string {
-        return $this->_genre;
-    }
-
-    public function setGenre($genre) : string {
-        return $this->_genre = $genre;
-    }
-
-    public function getDateNaissance() : DateTime {
-        return $this->_dateNaissance;
-    }
-
-    public function setDateNaissance($dateNaissance) {
-        return $this->_dateNaissance = $dateNaissance;
-    }
-
-    // toString
-
-    public function __toString() {
-        return $this->_prenom." ".$this->_nom;
-    }
-}
