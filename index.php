7d7bd6321fbed87e39a932cd8f98d21621df5258
angalland <angalland@laposte.net>
2023-04-24 15:04:00 +0200

    modification de la méthode construct

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <?php
 
 spl_autoload_register(function ($class_name ) {
-    require  'Classes/'. $class_name . '.php';
+    require_once  'Classes/'. $class_name . '.php';
 });
 
-$real1 = new Personne ("Nolan", "Christopher", "masculin", "30-07-1970" );
+
+
+$real1 = new PersonneRea ("Nolan", "Christopher", "masculin", "30-07-1970" );
 $batmanBegins = new Film ("Batman begins", "15-06-2005", 140, $real1 );
 $theDarkNight = new Film ("The dark night", "13-08-2008", 152, $real1);
 $interstellar = new Film ("Interstellar", "05-11-2014", 169, $real1);
@@ -22,7 +24,7 @@ $michaelKeaton = new Acteur("Michael", "Keaton");
 
 
 
-$casting1 = new Casting ($batmanBegins, $alfred, $michaelCaine,"01-03-2004", 5);
+$casting1 = new Casting ($batmanBegins, $alfred, $michaelCaine,"01-02-2007", 5);
 $casting2 = new Casting ($theDarkNight, $alfred, $michaelCaine, "15-04-2006", 4);
 $casting3 = new Casting ($interstellar, $proffesseurBrand, $michaelCaine, "01-01-2013", 6);
 $casting4 = new Casting ($batmanBegins, $batman, $christianBale, "01-03-2004", 5);
