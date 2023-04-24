40da4f3210feccad1e2ca4887cf33644ae3a5c40
angalland <angalland@laposte.net>
2023-04-24 12:00:37 +0200

    ajout d'une class personne

--- Classes/Realisateur.php
+++ Classes/Realisateur.php
@@ -4,9 +4,7 @@ class Realisateur {
 
     // argument
 
-    private Film $_film;
-
-    // Class heritage Personne
+    private
 
     // constructeur
 
@@ -14,57 +12,6 @@ class Realisateur {
         $this->_film->addRealisateur($this);
     }
 
-    class Personne extends Realisateur {
-
-        // argument 
-
-        private string $_nom;
-        private string $_prenom;
-        private string $_genre;
-        private DateTime $_dateNaissance;
-
-        public function __construct (Film $film, string $nom, string $prenom, string $genre, string $dateNaissance){
-            parent::__construct($film);
-            $this->_nom = $nom;
-            $this->_prenom = $prenom;
-            $this->_genre = $genre;
-            $this->_dateNaissance = new DateTime ($dateNaissance);
-        }
-
-        // Getter et Setter 
-
-        public function getNom() : string {
-            return $this->_nom;
-        }
-    
-        public function setNom($nom) : string {
-            return $this->_nom = $nom;
-        }
-
-        public function getPrenom() : string {
-            return $this->_prenom;
-        }
-    
-        public function setPrenom($prenom) : string {
-            return $this->_prenom = $prenom;
-        }
-
-        public function getGenre() : string {
-            return $this->_genre;
-        }
-    
-        public function setGenre($genre) : string {
-            return $this->_genre = $genre;
-        }
-
-        public function getDateNaissance() : DateTime {
-            return $this->_dateNaissance;
-        }
-    
-        public function setDateNaissance($dateNaissance) {
-            return $this->_dateNaissance = $dateNaissance;
-        }
-    }
 
 
 
