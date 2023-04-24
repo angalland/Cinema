602301c63680bb1a68f7313a32e3eb5cede41053
angalland <angalland@laposte.net>
2023-04-24 16:07:33 +0200

    ajout de la méthode toString dans Genre

--- index.php
+++ index.php
@@ -7,11 +7,20 @@ spl_autoload_register(function ($class_name ) {
 });
 
 $personne1 = new Personne ("Dicaprio", "Leonardo", "Masculin", "11-11-1974");
+
 $real1 = new Realisateur ("Dicaprio", "Leonardo", "Masculin", "11-11-1974");
-$film1 = new Film ("Avant le déluge","21-10-2016", 96, $real1);
+
+$genre1 = new Genre ("Documentaire");
+$genre2 = new Genre ("science fiction");
+$genre3 = new Genre ("Action");
+$genre4 = new Genre ("Commedie");
+
+$film1 = new Film ("Avant le déluge","21-10-2016", 96, $real1, $genre1);
+$film2 = new Film ("")
 
 echo $personne1;
-echo $real1;
-echo $film1->getRealisateur();
+echo $real1."</br>";
+echo $film1->getRealisateur()."</br>";
+echo $film1->getGenre()."</br>";
 
 
